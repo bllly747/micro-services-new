30e085e4d740d75ea514cac39dd2ceb05f345b30
billy <abongilebilly990301@gmail.com>
2024-03-02 11:42:32 +0200

    added a discovery server and updated the pom.xml with eureka-clients and server

--- product-service/src/main/java/com/services/productService/ProductServiceApplication.java
+++ product-service/src/main/java/com/services/productService/ProductServiceApplication.java
@@ -2,7 +2,10 @@ package com.services.productService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProductServiceApplication {
 
